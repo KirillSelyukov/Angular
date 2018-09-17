95f5aaf5ea1ec77773360850577380c545e1807d
Kirill Selyukov <kselukov@gmail.com>
2018-09-17 14:43:07 +0300

    Move routing onfiguration to separate module

--- routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -12,14 +12,25 @@ export class EditServerComponent implements OnInit {
   server: { id: number; name: string; status: string };
   serverName = '';
   serverStatus = '';
+  allowEdit = false;
 
-  constructor(private serversService: ServersService,
-    private route: ActivatedRoute) {}
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    const id = this.route.snapshot.params['id'];
+    this.route.queryParams.subscribe((params: Params) => {
+      this.allowEdit = params['allowEdit'] === '1' ? true : false;
+    });
+    this.server = this.serversService.getServer(+id);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
+
+    this.route.params.subscribe((params: Params) => {
+      this.server = this.serversService.getServer(+params['id']);
+    });
   }
 
   onUpdateServer() {
