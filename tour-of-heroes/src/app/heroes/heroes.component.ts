3fa95d6166221a031f058637c01a7da563cc55ea
Kyrylo Seliukov <kselukov@gmail.com>
2018-11-30 16:51:26 +0200

    Heroes app displays a list of heroes in a Master/Detail view

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from '../hero'
+import { Hero } from '../hero'
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -12,7 +13,11 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Windstorm'
   };
+  selectedHero: Hero
+  heroes = HEROES
   ngOnInit() {
   }
-
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
 }
