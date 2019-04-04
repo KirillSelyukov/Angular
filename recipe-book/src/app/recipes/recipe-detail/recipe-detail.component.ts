81fe767e49653292bbb1e080d0413a328b9098f0
Kyrylo Seliukov <kselukov@gmail.com>
2019-04-04 21:20:15 +0300

    pass ingredients to shopping list

--- recipe-book/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ recipe-book/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
+import { Route } from '@angular/compiler/src/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.css']
+  styleUrls: ['./recipe-detail.component.css'],
 })
 export class RecipeDetailComponent implements OnInit {
-
   @Input() recipe: Recipe;
-  constructor() { }
+  constructor(private slService: ShoppingListService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  add() {
+    this.slService.addIngredients(this.recipe.ingredients);
+    this.slService.ingredientsChanged.subscribe((ingredients: Ingredient[]) =>
+      console.log(ingredients),
+    );
   }
-
 }
