f1ace85c02cc32c13cedabe9b7b128e9a935fa53
Kyrylo Seliukov <kselukov@gmail.com>
2019-04-17 18:08:15 +0300

    add validation to the form

--- recipe-book/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ recipe-book/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,11 +1,7 @@
-import {
-  Component,
-  OnInit,
-  ElementRef,
-  ViewChild
-} from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -13,16 +9,13 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput') nameInputRef: ElementRef;
-  @ViewChild('amountInput') amountInputRef: ElementRef;
   constructor(private service: ShoppingListService) {}
 
   ngOnInit() {}
 
-  onAddItem() {
-    const name = this.nameInputRef.nativeElement.value;
-    const amount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(name, amount);
+  onAddItem({ value }: NgForm) {
+    console.log(value);
+    const newIngredient = new Ingredient(value.name, value.amount);
 
     this.service.addIngredient(newIngredient);
   }
